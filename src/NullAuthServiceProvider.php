85fc909fcbc8608475b0c1ec1c6de11e9c52a692
mpyw <ryosuke_i_628@yahoo.co.jp>
2019-12-21 03:26:04 +0900

    Refactor: Simplify provider

--- src/NullAuthServiceProvider.php
+++ src/NullAuthServiceProvider.php
@@ -9,24 +9,18 @@ use Illuminate\Support\ServiceProvider;
 
 class NullAuthServiceProvider extends ServiceProvider
 {
-    /** @noinspection PhpDocMissingThrowsInspection */
-
     /**
      * @return void
      */
     public function register()
     {
-        /* @noinspection PhpUnhandledExceptionInspection */
-        $this->app->resolved(AuthManager::class)
-            ? static::extendComponents($this->app->make(AuthManager::class)) // @codeCoverageIgnore
-            : $this->app->afterResolving(AuthManager::class, Closure::fromCallable([$this, 'extendComponents']));
+        $this->app->afterResolving(AuthManager::class, Closure::fromCallable([$this, 'extendComponents']));
     }
 
     /**
-     * @param  \Illuminate\Auth\AuthManager $auth
-     * @return void
+     * @param \Illuminate\Auth\AuthManager $auth
      */
-    protected function extendComponents(AuthManager $auth)
+    protected function extendComponents(AuthManager $auth): void
     {
         $auth->extend('null', function (Container $app, string $name, array $config) use ($auth) {
             $guard = new NullGuard();
